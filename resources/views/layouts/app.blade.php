df94675866e4209f7937c49834d3084ed423a6b8
pedro.henrique <pedro.henrique@mobs2.com>
2020-07-23 11:11:05 -0300

    implementação de Versão

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,6 @@
 ﻿<!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
+<?php define('Version', '1'); ?>
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -12,14 +12,14 @@
     <link rel="icon" href="/img/favicon.png" type="image/png" />
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}?v=<?=Version?>" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}?v=<?=Version?>" rel="stylesheet">
 
     <style type="text/css" rel="stylesheet">
         .info {
